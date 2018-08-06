3778258c790d23081399ce41ad9f380a54da91a4
tangxi <tangxi669@163.com>
2018-08-06 14:14:58 +0800

    get parameter from database

--- src/main/java/base/DataProvider.java
+++ src/main/java/base/DataProvider.java
@@ -3,33 +3,45 @@ package base;
 import java.sql.*;
 
 public class DataProvider {
-    public static void main(String[] args) {
+    public static Connection connData() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        }catch (Exception ex){
-
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
         Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://192.168.31.100:3306/z_plan_test?" +
+                    "user=root&password=HkilDiYoPRwjh");
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        }
+        return conn;
+    }
+    public static String getParameters(String method, String testType) {
+        Connection conn = connData();
         Statement stmt = null;
         ResultSet rs = null;
-        try{
-            conn =
-                    DriverManager.getConnection("jdbc:mysql://192.168.31.100:3306/z_plan_test?"+
-                                                 "user=root&password=HkilDiYoPRwjh");
-            if(conn != null){
+        String r = null;
+        try {
+            if (conn != null) {
                 System.out.println("数据库连接正常！");
                 stmt = conn.createStatement();
-                rs = stmt.executeQuery("SELECT * FROM bbd_testcases ");
-                while(rs.next()){
-                    System.out.println(rs.getString("id"));
-                    System.out.println(rs.getString("servicename"));
+                rs = stmt.executeQuery("SELECT * FROM bbd_testcases WHERE Method = "+"\""+method+"\""+"AND testType = "+"\""+testType+"\"");
+
+                while (rs.next()) {
+                    r = rs.getString("Parameters");
+                    System.out.println(r);
                 }
-            }
-            else{
+            } else {
                 System.out.println("数据库连接异常");
             }
 
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
@@ -47,5 +59,9 @@ public class DataProvider {
                 stmt = null;
             }
         }
+        return r;
+    }
+    public static void main(String[] args) {
+        getParameters("archiveQueryCharts", "important");
     }
 }
